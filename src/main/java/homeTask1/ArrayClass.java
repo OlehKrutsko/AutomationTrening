867e23c40c2bf5ef0367ac532b7746f04c2b8610
OlehKrutsko <rembotito@ukr.net>
2017-05-26 14:17:31 +0300

    Initial commit

---
+++ src/main/java/homeTask1/ArrayClass.java
@@ -0,0 +1,10 @@
+package homeTask1;
+
+public class ArrayClass {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/main/java/homeTask1/StringClass.java
@@ -0,0 +1,24 @@
+package homeTask1;
+
+public class StringClass {
+
+
+	
+	
+public String concatenation (String str1){
+	return str1 = str1.concat("all the time");
+	
+}
+
+public String replacement (String str1){
+	return str1 = str1.replace("test", "hoho");
+	
+}
+
+	/*   public static void main(String args[]) {
+		   StringClass s = new StringClass();
+				   System.out.println(s.concatenation("I'm right "));  
+				   System.out.println(s.replacement("test test test and nothing more")); 
+	   }
+*/
+}
